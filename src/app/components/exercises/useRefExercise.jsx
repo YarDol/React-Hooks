35c8f540007a18231dc48b5f3ffb03766ebce200
Yar <slvwinswim@gmail.com>
2023-11-20 17:51:10 +0200

    useRef exercise

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useRef} from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    // const changeData = () => {
+    //     const block = document.querySelector(".bg-primary");
+    //     block.innerHTML = "text";
+    //     block.style.height = "150px";
+    //     block.style.width = "80px";
+    // };
+    const inputRef = useRef(null);
+
+    const changeData = () => {
+        inputRef.current.style.width = "150px";
+        inputRef.current.style.height = "80px";
+        inputRef.current.innerHTML = "text";
+    };
     return (
         <CollapseWrapper title="Tasks">
             <p className="mt-3">
@@ -11,7 +24,7 @@ const UseRefExercise = () => {
                 <li>Зміниться вміст блоку на &quot;text&quot;</li>
                 <li>висота і ширина дорівнюватимуть 150 і 80 відповідно</li>
             </ul>
-            <div
+            <div ref={inputRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
@@ -19,8 +32,9 @@ const UseRefExercise = () => {
                     color: "white"
                 }}
             >
-                <small>Блок</small>
+                <small ref={inputRef}>Блок</small>
             </div>
+            <button className="mt-3 me-3 btn btn-secondary" onClick={changeData}>Change Data</button>
         </CollapseWrapper>
     );
 };
