ff6dbde244ceba38f64d1fc91a43033386dede2d
Yar <slvwinswim@gmail.com>
2023-11-21 16:09:13 +0200

    reuse form creation

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { validator } from "../../../utils/validator";
 import api from "../../../api";
-// import TextField from "../../common/form/textField";
-// import SelectField from "../../common/form/selectField";
-// import RadioField from "../../common/form/radioField";
-// import MultiSelectField from "../../common/form/multiSelectField";
+import FormComponent, { TextField, SelectField } from "../../common/form";
 
 const EditUserPage = () => {
     const { userId } = useParams();
@@ -14,80 +10,36 @@ const EditUserPage = () => {
     const [data, setData] = useState({
         name: "",
         email: "",
-        profession: "",
-        sex: "male",
-        qualities: []
+        profession: ""
     });
     const [professions, setProfession] = useState([]);
-    const [qualities, setQualities] = useState({});
-    const [errors, setErrors] = useState({});
+
     const getProfessionById = (id) => {
-        for (const prof of professions) {
-            if (prof.value === id) {
-                return { _id: prof.value, name: prof.label };
-            }
+        for (const prof in professions) {
+            const profData = professions[prof];
+            if (profData._id === id) return profData;
         }
     };
-    const getQualities = (elements) => {
-        const qualitiesArray = [];
-        for (const elem of elements) {
-            for (const quality in qualities) {
-                if (elem.value === qualities[quality]._id) {
-                    qualitiesArray.push(qualities[quality]);
-                }
-            }
-        }
-        return qualitiesArray;
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isValid = validate();
-        if (!isValid) return;
-        const { profession, qualities } = data;
+
+    const handleSubmit = (data) => {
+        const { profession } = data;
         api.users
             .update(userId, {
                 ...data,
-                profession: getProfessionById(profession),
-                qualities: getQualities(qualities)
+                profession: getProfessionById(profession)
             })
             .then((data) => history.push(`/users/${data._id}`));
-        // console.log({
-        //     ...data,
-        //     profession: getProfessionById(profession),
-        //     qualities: getQualities(qualities)
-        // });
-    };
-    const transformData = (data) => {
-        return data.map((qual) => ({ label: qual.name, value: qual._id }));
     };
     useEffect(() => {
         setIsLoading(true);
-        api.users.getById(userId).then(({ profession, qualities, ...data }) =>
+        api.users.getById(userId).then(({ profession, ...data }) =>
             setData((prevState) => ({
                 ...prevState,
                 ...data,
-                qualities: transformData(qualities),
                 profession: profession._id
             }))
         );
-        
-        api.professions.fetchAll().then((data) => {
-            const professionsList = Object.keys(data).map((professionName) => ({
-                label: data[professionName].name,
-                value: data[professionName]._id
-            }));
-            // console.log(professionsList);
-            setProfession(professionsList);
-        });
-        // api.professions.fetchAll().then((data) => {});
-        api.qualities.fetchAll().then((data) => {
-            const qualitiesList = Object.keys(data).map((optionName) => ({
-                value: data[optionName]._id,
-                label: data[optionName].name,
-                color: data[optionName].color
-            }));
-            setQualities(qualitiesList);
-        });
+        api.professions.fetchAll().then((data) => setProfession(data));
     }, []);
     useEffect(() => {
         if (data._id) setIsLoading(false);
@@ -108,76 +60,33 @@ const EditUserPage = () => {
             }
         }
     };
-    useEffect(() => {
-        validate();
-    }, [data]);
-    // const handleChange = (target) => {
-    //     setData((prevState) => ({
-    //         ...prevState,
-    //         [target.name]: target.value
-    //     }));
-    // };
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
-    // const isValid = Object.keys(errors).length === 0;
+
     return (
         <div className="container mt-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3 shadow p-4">
                     {!isLoading && Object.keys(professions).length > 0 ? (
-                        <form onSubmit={handleSubmit}>
-                            {/* <TextField
-                                label="Имя"
-                                name="name"
-                                value={data.name}
-                                onChange={handleChange}
-                                error={errors.name}
-                            />
-                            <TextField
-                                label="Электронная почта"
-                                name="email"
-                                value={data.email}
-                                onChange={handleChange}
-                                error={errors.email}
-                            />
+                        <FormComponent
+                            onSubmit={handleSubmit}
+                            validatorConfig={validatorConfig}
+                            defaultData={data}
+                        >
+                            <TextField label="Имя" name="name" autoFocus />
+                            <TextField label="Электронная почта" name="email" />
                             <SelectField
-                                label="Profession" 
-                                value={data.profession} 
-                                onChange={handleChange} 
-                                defaultOption="Choose..." 
-                                options={professions} 
+                                label="Выбери свою профессию"
+                                defaultOption="Choose..."
+                                options={professions}
                                 name="profession"
-                                error={errors.profession}
                             />
-                            <RadioField
-                                options={[
-                                    { name: "Male", value: "male" },
-                                    { name: "Female", value: "female" },
-                                    { name: "Other", value: "other" }
-                                ]}
-                                value={data.sex}
-                                name="sex"
-                                onChange={handleChange}
-                                label="Выберите ваш пол"
-                            />
-                            <MultiSelectField
-                                defaultValue={data.qualities}
-                                options={qualities}
-                                onChange={handleChange}
-                                name="qualities"
-                                label="Выберите ваши качества"
-                            /> */}
-                            {/* <button
+
+                            <button
                                 type="submit"
-                                disabled={!isValid}
                                 className="btn btn-primary w-100 mx-auto"
                             >
-                                Оновити
-                            </button> */}
-                        </form>
+                                Обновить
+                            </button>
+                        </FormComponent>
                     ) : (
                         "Loading..."
                     )}
